fa0bbb2ee0e14a907ac11ed39a5bb4abc52c555c
Omar Khan <omrkhan@amazon.co.uk>
2022-10-26 13:21:47 +0000

    script to upload resources to s3 finalised

--- provision_resources.sh
+++ provision_resources.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
+set -e
 
 KRAKEN_DB='k2_pluspf_16gb_20220607.tar.gz'
 KRAKEN_DB_URL='https://genome-idx.s3.amazonaws.com/kraken'
-KRAKEN_DB_S3_URI='s3://tbpipeline/kraken_db'
+KRAKEN_DB_S3_URL='s3://tbpipeline/kraken_db/k2_pluspf_16gb_20220607/'
 
-BOWTIE2_INDEX='hg19_1kgmaj'
-BOWTIE2_INDEX_URL="https://genome-idx.s3.amazonaws.com/bt/${BOWTIE2_INDEX}_snvs_bt2.zip"
-BOWTIE2_INDEX_S3_URI='s3://tbpipeline/bowtie2_index'
+BOWTIE2_INDEX='hg19_1kgmaj_snvs_bt2.zip'
+BOWTIE2_INDEX_URL='https://genome-idx.s3.amazonaws.com/bt'
+BOWTIE2_INDEX_S3_URI='s3://tbpipeline/bowtie2_index/hg19_1kgmaj/'
 
-RESOURCE_S3_URI='s3://tbpipeline/resources/'
 
-wget "${KRAKEN_DB_URL}/${KRAKEN_DB}"
-tar xzf $KRAKEN_DB -C ./k2db
-aws s3 cp ./k2db/* "$S3_URI/${KRAKEN_DB}"
-rm -r ./k2db
+wget -c "${KRAKEN_DB_URL}/${KRAKEN_DB}"
+TMPDIR=$(mktemp -d) #runs the command and prints the name
+mkdir $TMPDIR/k2db
+tar xzf $KRAKEN_DB -C $TMPDIR/k2db
+aws s3 sync $TMPDIR/k2db $KRAKEN_DB_S3_URL
+rm -r $TMPDIR/k2db
+rm $KRAKEN_DB
 
-wget "${BOWTIE2_INDEX_URL}/${BOWTIE2_INDEX}"
-unzip ${BOWTIE2_INDEX}_snvs_bt2
-aws s3 cp ./${BOWTIE2_INDEX}_snvs_bt2/* "$BOWTIE2_INDEX_S3_URI/${BOWTIE2_INDEX}"
-rm -r ./${BOWTIE2_INDEX}_snvs_bt2
+wget -c "${BOWTIE2_INDEX_URL}/${BOWTIE2_INDEX}"
+mkdir $TMPDIR/bowtiedb
+unzip $BOWTIE2_INDEX -d $TMPDIR/bowtiedb
+aws s3 sync $TMPDIR/bowtiedb $BOWTIE2_INDEX_S3_URI
+rm -r bowtiedb
+rm $BOWTIE2_INDEX
 
-aws s3 cp ./resources/* $RESOURCE_S3_URI
+aws s3 sync tb-pipeline-resources s3://tbpipeline/resources/
