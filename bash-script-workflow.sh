83b7506ed57aa7a73489d007f244cef88c507334
Michael Mueller <micamue@amazon.co.uk>
2022-10-28 15:34:53 +0000

    tidy up dryrun-test-agc script

--- bash-script-workflow.sh
+++ bash-script-workflow.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
-echo "Generating scenario inputs and manifests"
+echo "Generating scenario inputs.json and MANIFEST.jsons"
 python ./tests/scrip-test/mainfest-script/manifest-automation.py
+
+# backup inputs.json and MANIFEST.json in project root directory
 mv inputs.json inputs.json.template
 mv MANIFEST.json MANIFEST.json.template
 
 for i in {1..10}; do
-   echo "Copying scenario $i" 
-   cp ./tests/scrip-test/mainfest-script/output/inputs.scenario${i}.json ./inputs.json
-   cp ./tests/scrip-test/mainfest-script/output/MANIFEST${i}.json ./MANIFEST.json
+    echo "Submitting workflow run for test scenario $i"
 
-   echo "Submitting workflow $i" 
-   #submit workflow here by calling the genetics cli
-   agc workflow run tbpipeline --context ondemand
-   echo "input.jsonscenario$i"
- done
-FILE=inputs.json
-if test -e "$FILE"; then
-    echo "rm inputs.json"
-fi
+    # copy scenario input.json and MANIFEST.json
+    cp ./tests/scrip-test/mainfest-script/output/inputs.scenario${i}.json ./inputs.json
+    cp ./tests/scrip-test/mainfest-script/output/MANIFEST${i}.json ./MANIFEST.json
 
-FILE2=MANIFEST.json
-if test -e "$FILE2"; then 
-    echo "rm MANIFEST.json"
-fi
- 
- # changes the manifest and input files from template to .json after all the test runs
- mv inputs.json.template inputs.json
- mv MANIFEST.json.template MANIFEST.json
- 
+    # submit workflow run
+    agc workflow run tbpipeline --context ondemand
+
+done
+
+# changes the manifest and input files from template to .json after all the test runs
+mv inputs.json.template inputs.json
+mv MANIFEST.json.template MANIFEST.json
