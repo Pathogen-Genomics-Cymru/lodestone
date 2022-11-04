ce44cbb14f26edd1d13ecf601643930f0a94b632
Maxim Filimonov <max.filimonov@gmail.com>
2022-11-04 11:40:45 +0000

    pytest adjusted after tax_ids changed to int type

--- tests/python/test_parse_kraken_report2.py
+++ tests/python/test_parse_kraken_report2.py
@@ -78,10 +78,10 @@ def test_read_and_parse_kraken_report():
     test_kraken_report = os.path.join(data_dir_name, kraken_txt)
     got_read = parse_kraken_report2.read_kraken_report(test_kraken_report, pct_threshold, num_threshold)
     expect = (
-        [[142104, 10.08, 'Mycobacterium tuberculosis', '1773'], [78001, 5.53, 'Mycobacterium intracellulare', '1767']],
-        [[1393167, 98.78, 'Mycobacterium', '1763']],
-        [[1124355, 79.72, 'Mycobacterium tuberculosis complex', '77643'], [122223, 8.67, 'Mycobacterium avium complex (MAC)', '120793']],
-        [[1404682, 99.6, 'Mycobacteriaceae', '1762']],
+        [[142104, 10.08, 'Mycobacterium tuberculosis', 1773], [78001, 5.53, 'Mycobacterium intracellulare', 1767]],
+        [[1393167, 98.78, 'Mycobacterium', 1763]],
+        [[1124355, 79.72, 'Mycobacterium tuberculosis complex', 77643], [122223, 8.67, 'Mycobacterium avium complex (MAC)', 120793]],
+        [[1404682, 99.6, 'Mycobacteriaceae', 1762]],
         2
     )
     assert got_read == expect
@@ -103,10 +103,10 @@ def test_read_and_parse_kraken_report2():
     test_kraken_report = os.path.join(data_dir_name, kraken_txt)
     got_read = parse_kraken_report2.read_kraken_report(test_kraken_report, pct_threshold, num_threshold)
     expect = (
-        [[48, 0.1, 'Human immunodeficiency virus 1', '11676']],
-        [[4275, 8.9, 'Corynebacterium', '1716'], [83, 0.17, 'Mycobacterium', '1763'], [48, 0.1, 'Lentivirus', '11646']],
+        [[48, 0.1, 'Human immunodeficiency virus 1', 11676]],
+        [[4275, 8.9, 'Corynebacterium', 1716], [83, 0.17, 'Mycobacterium', 1763], [48, 0.1, 'Lentivirus', 11646]],
         [],
-        [[4275, 8.9, 'Corynebacteriaceae', '1653'], [83, 0.17, 'Mycobacteriaceae', '1762'], [48, 0.1, 'Retroviridae', '11632']],
+        [[4275, 8.9, 'Corynebacteriaceae', 1653], [83, 0.17, 'Mycobacteriaceae', 1762], [48, 0.1, 'Retroviridae', 11632]],
         1
     )
     assert got_read == expect
@@ -128,10 +128,10 @@ def test_read_and_parse_kraken_report3():
     test_kraken_report = os.path.join(data_dir_name, kraken_txt)
     got_read = parse_kraken_report2.read_kraken_report(test_kraken_report, pct_threshold, num_threshold)
     expect = (
-        [[319481, 92.44, 'Mycobacterium abscessus', '36809'], [5097, 1.47, 'Mycobacterium tuberculosis', '1773']],
-        [[345582, 99.99, 'Mycobacterium', '1763']],
-        [[322723, 93.38, 'Mycobacterium chelonae group', '670516'], [7442, 2.15, 'Mycobacterium tuberculosis complex', '77643'], [5542, 1.6, 'Mycobacterium avium complex (MAC)', '120793']],
-        [[345582, 99.99, 'Mycobacteriaceae', '1762']],
+        [[319481, 92.44, 'Mycobacterium abscessus', 36809], [5097, 1.47, 'Mycobacterium tuberculosis', 1773]],
+        [[345582, 99.99, 'Mycobacterium', 1763]],
+        [[322723, 93.38, 'Mycobacterium chelonae group', 670516], [7442, 2.15, 'Mycobacterium tuberculosis complex', 77643], [5542, 1.6, 'Mycobacterium avium complex (MAC)', 120793]],
+        [[345582, 99.99, 'Mycobacteriaceae', 1762]],
         2
     )
     assert got_read == expect
@@ -153,10 +153,10 @@ def test_read_and_parse_kraken_report4():
     test_kraken_report = os.path.join(data_dir_name, kraken_txt)
     got_read = parse_kraken_report2.read_kraken_report(test_kraken_report, pct_threshold, num_threshold)
     expect = (
-        [[21302, 18.5, 'Mycobacterium chimaera', '222805'], [9316, 8.09, 'Mycobacterium tuberculosis', '1773'], [2713, 2.36, 'Mycobacterium bovis', '1765'], [1097, 0.95, 'Mycobacterium nonchromogenicum', '1782'], [1097, 0.95, 'Mycobacterium kumamotonense', '354243'], [1165, 1.01, 'Mycobacterium leprae', '1769']],
-        [[114967, 99.84, 'Mycobacterium', '1763']],
-        [[43595, 37.86, 'Mycobacterium avium complex (MAC)', '120793'], [13385, 11.62, 'Mycobacterium tuberculosis complex', '77643'], [2317, 2.01, 'Mycobacterium terrae complex', '1073531'], [1080, 0.94, 'Mycobacterium chelonae group', '670516']],
-        [[114967, 99.84, 'Mycobacteriaceae', '1762']],
+        [[21302, 18.5, 'Mycobacterium chimaera', 222805], [9316, 8.09, 'Mycobacterium tuberculosis', 1773], [2713, 2.36, 'Mycobacterium bovis', 1765], [1097, 0.95, 'Mycobacterium nonchromogenicum', 1782], [1097, 0.95, 'Mycobacterium kumamotonense', 354243], [1165, 1.01, 'Mycobacterium leprae', 1769]],
+        [[114967, 99.84, 'Mycobacterium', 1763]],
+        [[43595, 37.86, 'Mycobacterium avium complex (MAC)', 120793], [13385, 11.62, 'Mycobacterium tuberculosis complex', 77643], [2317, 2.01, 'Mycobacterium terrae complex', 1073531], [1080, 0.94, 'Mycobacterium chelonae group', 670516]],
+        [[114967, 99.84, 'Mycobacteriaceae', 1762]],
         6
     )
     assert got_read == expect
@@ -179,9 +179,9 @@ def test_read_and_parse_kraken_report5():
     got_read = parse_kraken_report2.read_kraken_report(test_kraken_report, pct_threshold, num_threshold)
     expect = (
         [],
-        [[1456, 100.0, 'Mycobacterium', '1763']],
-        [[1076, 73.9, 'Mycobacterium tuberculosis complex', '77643']],
-        [[1456, 100.0, 'Mycobacteriaceae', '1762']],
+        [[1456, 100.0, 'Mycobacterium', 1763]],
+        [[1076, 73.9, 'Mycobacterium tuberculosis complex', 77643]],
+        [[1456, 100.0, 'Mycobacteriaceae', 1762]],
         0
     )
     assert got_read == expect
@@ -204,9 +204,9 @@ def test_read_and_parse_kraken_report6():
     got_read = parse_kraken_report2.read_kraken_report(test_kraken_report, pct_threshold, num_threshold)
     expect = (
         [],
-        [[7292, 80.27, 'Mycobacterium', '1763']],
-        [[4514, 49.69, 'Mycobacterium tuberculosis complex', '77643']],
-        [[7292, 80.27, 'Mycobacteriaceae', '1762']],
+        [[7292, 80.27, 'Mycobacterium', 1763]],
+        [[4514, 49.69, 'Mycobacterium tuberculosis complex', 77643]],
+        [[7292, 80.27, 'Mycobacteriaceae', 1762]],
         0
     )
     assert got_read == expect
@@ -228,10 +228,10 @@ def test_read_and_parse_kraken_report7():
     test_kraken_report = os.path.join(data_dir_name, kraken_txt)
     got_read = parse_kraken_report2.read_kraken_report(test_kraken_report, pct_threshold, num_threshold)
     expect = (
-        [[140109, 99.17, 'Mycobacterium tuberculosis', '1773']],
-        [[140109, 99.17, 'Mycobacterium', '1763']],
-        [[140109, 99.17, 'Mycobacterium tuberculosis complex', '77643']],
-        [[140109, 99.17, 'Mycobacteriaceae', '1762']],
+        [[140109, 99.17, 'Mycobacterium tuberculosis', 1773]],
+        [[140109, 99.17, 'Mycobacterium', 1763]],
+        [[140109, 99.17, 'Mycobacterium tuberculosis complex', 77643]],
+        [[140109, 99.17, 'Mycobacteriaceae', 1762]],
         1
     )
     assert got_read == expect
