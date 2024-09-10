402a4ccd54b4174454599bd77ae1b07cb8f11c15
Tom Whalley <whalleyt@cardiff.ac.uk>
2024-09-10 13:52:54 +0000

    change regex

--- bin/identify_tophit_and_contaminants2.py
+++ bin/identify_tophit_and_contaminants2.py
@@ -358,9 +358,12 @@ def process_reports(afanc_json_path, kraken_json_path, supposed_species, unmix_m
         out['summary_questions']['were_contaminants_removed'] = 'no'
 
     # IS THE TOP SPECIES HIT ONE OF THE 10 ACCEPTABLE POSSIBILITIES? IF SO, PROVIDE A LINK TO THE REFERENCE GENOME
-    re_top_species = re.findall(r"^(Mycobact|Mycolicibac)\w+ (abscessus|africanum|avium|bovis|chelonae|chimaera|fortuitum|intracellulare|kansasii|tuberculosis).*?$", top_species)
+    re_top_species = re.findall(r"^(Mycobact|Mycolicibac)\w+ (abscessus|africanum|avium|bovis|chelonae|chimaera|fortuitum|intracellulare|kansasii|tuberculosis) ()\w+ (bovis|orgis|caprae).*?$", top_species)
     if len(re_top_species) > 0:
-        identified_species = re_top_species[0][1]
+        if len(re_top_species[0]) == 2:
+            identified_species = re_top_species[0][1]
+        else:
+            identified_species = re_top_species[0][3] #we have bovis (or  orgis/caprae) with variant in the name
         if supposed_species == 'null':
             out['summary_questions']['is_the_top_species_appropriate'] = 'yes'
         elif ((supposed_species != 'null') & (supposed_species == identified_species)):
