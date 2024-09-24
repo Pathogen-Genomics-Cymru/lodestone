2552081864827643325c3b1485ff78552aadf400
Tom Whalley <whalleyt@cardiff.ac.uk>
2024-09-24 08:33:04 +0000

    update test

--- tests/python/test_parse_kraken_report2.py
+++ tests/python/test_parse_kraken_report2.py
@@ -80,7 +80,7 @@ def test_read_and_parse_kraken_report():
     expect = (
         [[142104, 10.08, 'Mycobacterium tuberculosis', 1773], [78001, 5.53, 'Mycobacterium intracellulare', 1767]],
         [[1393167, 98.78, 'Mycobacterium', 1763]],
-        [[1124355, 79.72, 'Mycobacterium tuberculosis complex', 77643], [122223, 8.67, 'Mycobacterium avium complex (MAC)', 120793]],
+        [[1124355, 79.72, 'Mycobacterium tuberculosis complex', 77643], [122223, 8.67, 'Mycobacterium avium complex MAC', 120793]],
         [[1404682, 99.6, 'Mycobacteriaceae', 1762]],
         2
     )
@@ -130,7 +130,7 @@ def test_read_and_parse_kraken_report3():
     expect = (
         [[319481, 92.44, 'Mycobacterium abscessus', 36809], [5097, 1.47, 'Mycobacterium tuberculosis', 1773]],
         [[345582, 99.99, 'Mycobacterium', 1763]],
-        [[322723, 93.38, 'Mycobacterium chelonae group', 670516], [7442, 2.15, 'Mycobacterium tuberculosis complex', 77643], [5542, 1.6, 'Mycobacterium avium complex (MAC)', 120793]],
+        [[322723, 93.38, 'Mycobacterium chelonae group', 670516], [7442, 2.15, 'Mycobacterium tuberculosis complex', 77643], [5542, 1.6, 'Mycobacterium avium complex MAC', 120793]],
         [[345582, 99.99, 'Mycobacteriaceae', 1762]],
         2
     )
@@ -155,7 +155,7 @@ def test_read_and_parse_kraken_report4():
     expect = (
         [[21302, 18.5, 'Mycobacterium chimaera', 222805], [9316, 8.09, 'Mycobacterium tuberculosis', 1773], [2713, 2.36, 'Mycobacterium bovis', 1765], [1097, 0.95, 'Mycobacterium nonchromogenicum', 1782], [1097, 0.95, 'Mycobacterium kumamotonense', 354243], [1165, 1.01, 'Mycobacterium leprae', 1769]],
         [[114967, 99.84, 'Mycobacterium', 1763]],
-        [[43595, 37.86, 'Mycobacterium avium complex (MAC)', 120793], [13385, 11.62, 'Mycobacterium tuberculosis complex', 77643], [2317, 2.01, 'Mycobacterium terrae complex', 1073531], [1080, 0.94, 'Mycobacterium chelonae group', 670516]],
+        [[43595, 37.86, 'Mycobacterium avium complex MAC', 120793], [13385, 11.62, 'Mycobacterium tuberculosis complex', 77643], [2317, 2.01, 'Mycobacterium terrae complex', 1073531], [1080, 0.94, 'Mycobacterium chelonae group', 670516]],
         [[114967, 99.84, 'Mycobacteriaceae', 1762]],
         6
     )
